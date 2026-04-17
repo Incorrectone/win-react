6448851d3bc774a63b8c9586d450ca4329db99cf
incor <incorrectmail101@gmail.com>
2026-04-17 14:10:53 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,38 @@
+import { useState } from 'react'
+import { Rnd } from "react-rnd";
+import './App.css'
+
+const style = {
+  display: "flex",
+  alignItems: "center",
+  justifyContent: "center",
+  border: "solid 1px #ddd",
+  background: "#f0f0f0",
+};
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <section id="center">
+        <p> Hi </p>
+
+
+        <Rnd
+        style={style}
+          default={{
+            x: 0,
+            y: 0,
+            width: 320,
+            height: 200,
+          }}
+        >
+          Rnd
+        </Rnd>
+      </section>
+    </>
+  )
+}
+
+export default App
